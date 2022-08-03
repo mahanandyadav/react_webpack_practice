15783ff711146af45784a900b2a5c308f7c3ea29
mahanandyadav <mahanandyadav04@gmail.com>
2022-08-03 13:02:08 +0530

    react form scratch

--- ui/src/components/myComponent.js
+++ ui/src/components/myComponent.js
@@ -4,34 +4,17 @@ import React from "react";
 class MyComponent extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            fav: 'yellow'
-        }
+        
     }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState  ({
-                // fav: 'pink'
-            })
-        }, 1000)
-    }
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        document.getElementById("div1").innerHTML="pev color" + prevState.fav;
-        return null
-    }
-    componentDidUpdate() {
-        document.getElementById("div2").innerHTML="current color" + this.state.fav
-    }
+   
 
 
 
     render() {
         return (
             <div>
-                <h1>{this.state.fav}</h1>
-                <div id="div1"  ></div>
-                <div id="div2" ></div>
+                <h1>hello2</h1>
             </div>
         )
     }
