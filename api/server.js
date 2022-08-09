6975bec782f4a3d5be5478e45f5380265bff7d5c
mahanandyadav <mahanandyadav04@gmail.com>
2022-08-09 11:35:52 +0530

    before pm2 installation

--- api/server.js
+++ api/server.js
@@ -1,20 +1,40 @@
-const express=require('express')
+const cluster = require('cluster')
+process.env.UV_THREADPOOL_SIZE=1
 
-const app=express()
-require('./src/db/db')
+if (cluster.isMaster) {
+    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+} else {
 
-const loginRoute=require('./src/routes/login')
+    const express = require('express')
+    const app = express()
+    const crypto =require('crypto')
+    app.use(express.json())
+
+   
+    app.get('/', (req, res) => {
+
+        const start =Date.now()
+        crypto.pbkdf2('a','b',100000,512,'sha512',()=>{
+            console.log('1',Date.now()-start)
+            res.send('hello')
+        })
+        
+    })
+
+    app.get('/fast',(req,res)=>{
+        res.send('fast')
+    })
+
+    const port = process.env.PORT || 3000
+    app.listen(port, () => {
+        console.log(`listening on port ${port}`)
+    })
 
 
-app.use(express.json())
-app.use(loginRoute)
-
-
-
-const port=process.env.PORT || 3000
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`)
-})
+}
 
 
 
